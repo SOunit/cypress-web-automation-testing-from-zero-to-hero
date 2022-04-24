70c9c6bb7774e1bf77373c53b8a818b6cb89a6eb
SOunit <workbookwork7@gmail.com>
2022-04-24 12:00:35 -0700

    add iiteration sample

--- 01_ngx-cypress-test-master/cypress/integration/firstTest.spec.js
+++ 01_ngx-cypress-test-master/cypress/integration/firstTest.spec.js
@@ -61,7 +61,7 @@ describe('first', () => {
     cy.contains('nb-card', 'Horizontal form').find('[type="email"]');
   });
 
-  it.only('then and wrap methods', () => {
+  it('then and wrap methods', () => {
     cy.visit('/');
     cy.contains('Forms').click();
     cy.contains('Form Layouts').click();
@@ -120,6 +120,128 @@ describe('first', () => {
         cy.wrap(secondForm)
           .find('[for="exampleInputPassword1"]')
           .should('contain', 'Password');
+      });
+    });
+  });
+
+  it('invoke command', () => {
+    cy.visit('/');
+    cy.contains('Forms').click();
+    cy.contains('Form Layouts').click();
+
+    // 1
+    cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address');
+
+    // 2
+    cy.get('[for="exampleInputEmail1"]').then((label) => {
+      expect(label.text()).to.equal('Email address');
+    });
+
+    // 3
+    cy.get('[for="exampleInputEmail1"]')
+      .invoke('text')
+      .then((text) => {
+        expect(text).to.equal('Email address');
+      });
+
+    // 4
+    cy.contains('nb-card', 'Basic form')
+      .find('nb-checkbox')
+      .click()
+      .find('.custom-checkbox')
+      .invoke('attr', 'class')
+      // 4.1
+      // .should('contain', 'checked');
+      // 4.2
+      .then((classValue) => {
+        expect(classValue).to.contain('checked');
+      });
+  });
+
+  it('assert property', () => {
+    cy.visit('/');
+    cy.contains('Forms').click();
+    cy.contains('Datepicker').click();
+
+    cy.contains('nb-card', 'Common Datepicker')
+      .find('input')
+      .then((input) => {
+        cy.wrap(input).click();
+        cy.get('nb-calendar-day-picker').contains('17').click();
+        cy.wrap(input)
+          .invoke('prop', 'value')
+          .should('contain', 'Apr 17, 2022');
+      });
+  });
+
+  it('radio button', () => {
+    cy.visit('/');
+    cy.contains('Forms').click();
+    cy.contains('Form Layouts').click();
+
+    cy.contains('nb-card', 'Using the Grid')
+      .find('[type="radio"]')
+      .then((radioButtons) => {
+        cy.wrap(radioButtons)
+          .first()
+          .check({ force: true })
+          .should('be.checked');
+
+        cy.wrap(radioButtons).eq(1).check({ force: true });
+
+        cy.wrap(radioButtons).eq(0).should('not.be.checked');
+
+        cy.wrap(radioButtons).eq(2).should('be.disabled');
+      });
+  });
+
+  it('check boxes', () => {
+    cy.visit('/');
+    cy.contains('Modal & Overlays').click();
+    cy.contains('Toastr').click();
+
+    // cy.get('[type="checkbox"]').check({ force: true });
+    cy.get('[type="checkbox"]').eq(0).click({ force: true });
+    cy.get('[type="checkbox"]').eq(0).check({ force: true });
+  });
+
+  it.only('lists and dropdown', () => {
+    cy.visit('/');
+
+    // 1st
+    // cy.get('nav nb-select').click();
+    // cy.get('.options-list').contains('Dark').click();
+    // cy.get('nav nb-select').should('contain', 'Dark');
+    // cy.get('nb-layout-header nav').should(
+    //   'have.css',
+    //   'background-color',
+    //   'rgb(34, 43, 69)'
+    // );
+
+    // 2nd
+    cy.get('nav nb-select').then((dropdown) => {
+      cy.wrap(dropdown).click();
+
+      cy.get('.options-list nb-option').each((listItem, index) => {
+        const itemText = listItem.text().trim();
+
+        const colors = {
+          Light: 'rgb(255, 255, 255)',
+          Dark: 'rgb(34, 43, 69)',
+          Cosmic: 'rgb(50, 50, 89)',
+          Corporate: 'rgb(255, 255, 255)',
+        };
+
+        cy.wrap(listItem).click();
+        cy.wrap(dropdown).should('contain', itemText);
+        cy.get('nb-layout-header nav').should(
+          'have.css',
+          'background-color',
+          colors[itemText]
+        );
+        if (index < 3) {
+          cy.wrap(dropdown).click();
+        }
       });
     });
   });
