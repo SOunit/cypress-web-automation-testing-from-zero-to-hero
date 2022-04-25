60d6e3dd32ade5fecb78ef272f08272f54d52f60
SOunit <workbookwork7@gmail.com>
2022-04-25 14:56:29 -0700

    add mock sample

--- 05_new_project/cypress/integration/firstTest.spec.js
+++ 05_new_project/cypress/integration/firstTest.spec.js
@@ -1,11 +1,30 @@
 /// <reference types="cypress" />
 
-describe("Test with backend", () => {
-  beforeEach("login to the app", () => {
+describe('Test with backend', () => {
+  beforeEach('login to the app', () => {
     cy.loginToApplication();
   });
 
-  it("should login", () => {
-    cy.log("Yes we login!");
+  it('verify correct request and response', () => {
+    cy.server();
+    cy.route('POST', '**/articles').as('postArticles');
+
+    cy.contains('New Article').click();
+    cy.get('[formcontrolname="title"]').type(
+      `This is a title ${Math.random()}`
+    );
+    cy.get('[formcontrolname="description"]').type('This is a description');
+    cy.get('[formcontrolname="body"]').type('This is a body');
+    cy.contains('Publish Article').click();
+
+    cy.wait('@postArticles');
+    cy.get('@postArticles').then((xhr) => {
+      console.log(xhr);
+      expect(xhr.status).to.equal(200);
+      expect(xhr.request.body.article.body).to.equal('This is a body');
+      expect(xhr.response.body.article.description).to.equal(
+        'This is a description'
+      );
+    });
   });
 });
